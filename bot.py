e62666ccf39625f695a6d4e275d12333084a76a9
andybeardness <beardness.andy@gmail.com>
2023-02-03 00:10:28 +0400

    minor fixes

--- bot.py
+++ bot.py
@@ -5,7 +5,31 @@ import reply_manager
 
 bot = telebot.TeleBot(secrets.TOKEN, parse_mode=None)
 
-@bot.message_handler(func=lambda _: True)
+CONTENT_TYPES = [
+	"text", 
+	"audio", 
+	"document", 
+	"photo", 
+	"sticker", 
+	"video", 
+	"video_note", 
+	"voice", 
+	"location", 
+	"contact", 
+	"new_chat_members", 
+	"left_chat_member", 
+	"new_chat_title", 
+	"new_chat_photo", 
+	"delete_chat_photo",
+	"group_chat_created", 
+	"supergroup_chat_created", 
+	"channel_chat_created", 
+	"migrate_to_chat_id",
+	"migrate_from_chat_id", 
+	"pinned_message",
+]
+
+@bot.message_handler(content_types=CONTENT_TYPES)
 def reply(message):
 	is_need_to_send_message = randomizer.is_need_to_send_message()
 
